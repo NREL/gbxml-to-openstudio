e07e07f72c6c7a3907bc4f3dc8f644a09078cfc5
pflaumingo <pflauming@gmail.com>
2018-08-30 15:31:46 -0600

    Testing pattern of wrapper objects that are then instantiated on demand

--- measures/gbxml_hvac_import/resources/hot_water_loop/hot_water_loop.rb
+++ measures/gbxml_hvac_import/resources/hot_water_loop/hot_water_loop.rb
@@ -1,18 +1,45 @@
 class HotWaterLoop
-  def self.create_hw_loop_from_xml(model, std, xml)
-    hw_loop = std.model_add_hw_loop(model, 'NaturalGas')
+  attr_accessor :model, :name, :references, :id, :cad_object_id
+
+  def initialize(model)
+    self.model = model
+    self.references = []
+  end
+
+  def set_name(name)
+    self.name = name
+  end
+
+  def set_id(id)
+    self.id = id
+  end
+
+  def set_cad_object_id(cad_object_id)
+    self.cad_object_id = cad_object_id
+  end
+
+  def add_reference(reference)
+    self.references.push(reference)
+  end
+
+  def build
+    hw_loop = OpenStudio::Model::PlantLoop.new(model)
+  end
+
+  def self.create_hw_loop_from_xml(model, xml)
+    hw_loop = new(model)
 
     name = xml.elements['Name']
     unless name.nil?
-      hw_loop.setName(xml.elements['Name'].text)
+      hw_loop.set_name(xml.elements['Name'].text)
     end
 
     unless xml.attributes['id'].nil?
-      hw_loop.additionalProperties.setFeature('id', xml.attributes['id'])
+      hw_loop.set_id(xml.attributes['id'])
     end
 
     unless xml.elements['CADObjectId'].nil?
-      hw_loop.additionalProperties.setFeature('CADObjectId', xml.elements['CADObjectId'].text)
+      hw_loop.set_cad_object_id(xml.elements['CADObjectId'].text)
     end
 
     hw_loop
