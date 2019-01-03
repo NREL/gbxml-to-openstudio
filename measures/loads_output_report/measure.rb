abf899bdc78bc0e78e1f3ac321128f1600ba4a19
Noah Pflaum <noah.pflaum@autodesk.com>
2019-01-03 09:36:37 -0700

    initial load output report

--- measures/loads_output_report/measure.rb
+++ measures/loads_output_report/measure.rb
@@ -4,8 +4,9 @@
 # http://nrel.github.io/OpenStudio-user-documentation/reference/measure_writing_guide/
 
 require 'openstudio'
+require_relative 'resources/component_load_summary'
 
-model = OpenStudio::Model::Model.new()
+# model = OpenStudio::Model::Model.new()
 
 # start the measure
 class LoadsOutputReport < OpenStudio::Measure::ReportingMeasure
@@ -33,7 +34,7 @@ class LoadsOutputReport < OpenStudio::Measure::ReportingMeasure
 
     return args
   end
-  
+
   # define the outputs that the measure will create
   def outputs
     outs = OpenStudio::Measure::OSOutputVector.new
@@ -42,7 +43,7 @@ class LoadsOutputReport < OpenStudio::Measure::ReportingMeasure
 
     return outs
   end
-  
+
   # return a vector of IdfObject's to request EnergyPlus objects needed by the run method
   # Warning: Do not change the name of this method to be snake_case. The method must be lowerCamelCase.
   def energyPlusOutputRequests(runner, user_arguments)
@@ -86,103 +87,38 @@ class LoadsOutputReport < OpenStudio::Measure::ReportingMeasure
     sql_file = sql_file.get
     model.setSqlFile(sql_file)
 
-    ## Get cooling peak load components for each space
-    base_query = "SELECT Value FROM TabularDataWithStrings"
-    load_row_names = ['People', 'Lights', 'Equipment', 'Refrigeration', 'Water Use Equipment', 'HVAC Equipment Losses', 'Power Generation Equipment',
-                 'DOAS Direct to Zone', 'Infiltration', 'Zone Ventilation', 'Interzone Mixing', 'Roof', 'Interzone Ceiling', 'Other Roof', 'Exterior Wall',
-                 'Interzone Wall', 'Ground Contact Wall', 'Other Wall', 'Exterior Floor', 'Interzone Floor', 'Ground Contact Floor', 'Other Floor',
-                 'Fenestration Conduction', 'Fenestration Solar', 'Opaque Door', 'Grand Total']
-    load_column_names = ['Sensible - Instant', 'Sensible - Delayed', 'Sensible - Return Air', 'Latent', 'Total']
-    peak_row_names =     ["Time of Peak Load", "Outside  Dry Bulb Temperature", "Outside  Wet Bulb Temperature", "Outside Humidity Ratio at Peak",
-                          "Zone Dry Bulb Temperature", "Zone Relative Humidity", "Zone Humidity Ratio at Peak", "Supply Air Temperature", "Mixed Air Temperature",
-                          "Main Fan Air Flow", "Outside Air Flow", "Peak Sensible Load with Sizing Factor", "Difference Due to Sizing Factor", "Peak Sensible Load",
-                          "Estimated Instant + Delayed Sensible Load", "Difference Between Peak and Estimated Sensible Load"]
-    peak_column_name = ['Value']
-
     component_loads = {}
+    component_load_summary = ComponentLoadSummary.new(sql_file)
 
-    model.getThermalZones().each do |zone|
+    ## Get Space and Zone Component Load Summary Data
+    model.getThermalZones.each do |zone|
       zone_name = zone.name.get
-      cooling_zone_query = base_query + " WHERE ReportForString == '#{zone_name.upcase}' AND TableName == 'Estimated Cooling Peak Load Components'"
-      heating_zone_query = base_query + " WHERE ReportForString == '#{zone_name.upcase}' AND TableName == 'Estimated Cooling Peak Load Components'"
-
-      space_loads = {"cooling": {"loads": {}, "peak_conditions": {}}, "heating": {"loads": {}, "peak_conditions": {}}}
-
-      ## Get cooling loads
-      load_row_names.each do |row_name|
-        row_loads = {}
-
-        load_column_names.each do |column_name|
-          load_query = cooling_zone_query + " AND RowName == '#{row_name}' AND ColumnName == '#{column_name}'"
-
-          load_result = sql_file.execAndReturnFirstDouble(load_query)
-
-          if load_result.is_initialized
-            row_loads[column_name] = load_result.get
-          end
-        end
-
-        space_loads[:cooling][:loads][row_name] = row_loads
-      end
-
-      # Get heating loads
-      load_row_names.each do |row_name|
-        row_loads = {}
-
-        load_column_names.each do |column_name|
-          load_query = heating_zone_query + " AND RowName == '#{row_name}' AND ColumnName == '#{column_name}'"
-
-          load_result = sql_file.execAndReturnFirstDouble(load_query)
-
-          if load_result.is_initialized
-            row_loads[column_name] = load_result.get
-          end
-        end
-
-        space_loads[:heating][:loads][row_name] = row_loads
-      end
-
-      ## Get peak load components
-      cooling_peak_conditions_query = base_query + " WHERE ReportForString == '#{zone_name.upcase}' AND TableName == 'Cooling Peak Conditions'"
-      heating_peak_conditions_query = base_query + " WHERE ReportForString == '#{zone_name.upcase}' AND TableName == 'Heating Peak Conditions'"
-
-      ## Get cooling peak conditions
-      peak_row_names.each do |row_name|
-
-        load_query = cooling_peak_conditions_query + " AND RowName == '#{row_name}' AND ColumnName == 'Value'"
-        runner.registerInfo(load_query)
-        load_result = sql_file.execAndReturnFirstDouble(load_query)
-
-        if load_result.is_initialized
-          space_loads[:cooling][:peak_conditions][row_name] = load_result.get
-        end
-      end
-
-      ## Get heating peak conditions
-      peak_row_names.each do |row_name|
-
-        load_query = heating_peak_conditions_query + " AND RowName == '#{row_name}' AND ColumnName == 'Value'"
-        runner.registerInfo(load_query)
-        load_result = sql_file.execAndReturnFirstDouble(load_query)
-
-        if load_result.is_initialized
-          space_loads[:heating][:peak_conditions][row_name] = load_result.get
-        end
-      end
+      loads_and_peaks = component_load_summary.get_loads_and_peak_conditions(zone_name.upcase)
 
       zone.spaces.each do |space|
         id = space.additionalProperties.getFeatureAsString('CADObjectId')
 
         if id.is_initialized
-          component_loads[id.get] = space_loads
+          component_loads[id.get] = loads_and_peaks
         end
       end
 
     end
 
-    ## Add Engineering Checks
-    #
-    ## Add Conditions at time of peak
+    ## Get Airloop Component load Summary Data
+    model.getAirLoopHVACs.each do |airloop|
+      airloop_name = airloop.name.get
+      id = airloop.additionalProperties.getFeatureAsString('CADObjectId')
+
+      if id.is_initialized
+        loads_and_peaks = component_load_summary.get_loads_and_peak_conditions(airloop_name.upcase)
+        component_loads[id.get] = loads_and_peaks
+      end
+    end
+
+    ## Get facility level component load summary data
+    component_loads['facility'] = component_load_summary.get_loads_and_peak_conditions('Facility')
+
 
     json_out = File.open("../loads_out.json", "w")
     json_out.write(component_loads.to_json)
