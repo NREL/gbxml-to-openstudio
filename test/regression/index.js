670c049732606584b5cb2145499b9d1ce4aae73f
Tim Coleman <tjc.coleman@gmail.com>
2022-08-22 14:02:33 -0600

    add in a github action to run a subset of gbxml regression test files

--- test/regression/index.js
+++ test/regression/index.js
@@ -8,6 +8,31 @@ import path from 'path';
 
 const threads = Number(process.env.THREADS || Math.max(1, os.cpus().length - 3));
 const osVersion = process.env.OS_VERSION;
+const args = process.argv;
+
+// test only a subset of the xml files. Used for faster feedback on CI.
+// default false
+var subset = false; 
+if (args.length > 2 ) {
+  if (args[2] == 'subset') {
+    console.log('Subset set to true. Only a subset of test files will be ran');
+    subset = true; 
+  }
+}
+
+// List of tests that run < 60 seconds
+const subsetTestFiles = ['3Nordea.xml',
+                         'ExteriorWindowRatioCW.xml',
+                         'Villa.xml',
+                         'Clerestory.xml',
+                         'House.xml',
+                         'Villa Spaces.xml',
+                         'ExteriorWindowRatioWindow.xml',
+                         'Roofs.xml',
+                         'Residential.xml',
+                         '11 Jay St.xml',
+                         '34 Emerson.xml'
+                        ]
 
 if (!osVersion) throw 'OS_VERSION missing from .env file'
 
@@ -26,8 +51,16 @@ const workflows = [];
 for (const {file} of files) {
   await mkdir(`../../workflows/regression-tests/${osVersion}/${file}/`, {recursive: true});
   const workflow = `../../workflows/regression-tests/${osVersion}/${file}/${file.replace(/\.xml/, '')}.osw`;
-  workflows.push(workflow);
+  if(subset) {
+    if (subsetTestFiles.includes(file)) {
+      workflows.push(workflow);
+    }
+  }
+  else {
+    workflows.push(workflow);
+  }
   await writeFile(workflow, osw.replace(/GBXML_INPUT\.xml/g, file));
+  
 }
 
 workflows.forEach(workflow => {
@@ -43,6 +76,10 @@ workflows.forEach(workflow => {
         });
       } else if (process.platform === 'darwin') {
         return execa(`/Applications/OpenStudio-${osVersion}/bin/openstudio`, ['run', '-w', workflow]).catch(() => {
+          console.error(`Error running ${workflow}`);
+        });
+      } else if (process.platform === 'linux') {
+        return execa(`/usr/local/bin/openstudio-${osVersion}`, ['run', '-w', workflow]).catch(() => {
           console.error(`Error running ${workflow}`);
         });
       } else {
