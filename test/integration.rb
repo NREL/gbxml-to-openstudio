df2883f669d5c460ce92cc589059f102da13ca89
Matt Steen <matthew.steen@nrel.gov>
2025-03-31 22:25:52 -0600

    rewrite

--- test/integration.rb
+++ test/integration.rb
@@ -1,31 +1,49 @@
 require 'fileutils'
+require 'openstudio'
+require 'parallel'
 
-path_datapoints = "build"
+# constants
+TEST_TYPE = ARGV[0]
+TEST_SIZE = ARGV[1]
+OS_VERSION = OpenStudio.openStudioVersion
+DIRNAME = File.dirname(__FILE__)
+FIXTURES_PATH = "#{DIRNAME}/fixtures"
+OUTPUT_PATH = "#{DIRNAME}/integration/output/#{TEST_TYPE}/#{OS_VERSION}"
+OSW_TEMPLATE = "#{DIRNAME}/integration/#{TEST_TYPE}.osw"
 
-# loop through resource files
-jobs = []
-results_directories = Dir.glob("#{path_datapoints}/*")
-results_directories.each do |directory|
-  puts "running #{directory}"
-  test_dir = "#{directory}/data_point.osw"
-  string = "openstudio run -w '#{test_dir}'"
-  if not File.file?(test_dir)
-    puts "data_point.osw not found for #{directory}"
-    next
-  end
-
-  # system(string)
-  jobs << string
-
+case TEST_SIZE
+when 'large'
+  # all files
+  fixtures = Dir.children(FIXTURES_PATH)
+when 'small'
+  # files < 1000 KB
+  fixtures = []
+  Dir.children(FIXTURES_PATH).each { |f| fixtures << f if File.size("#{FIXTURES_PATH}/#{f}") <= 1000000 }
+else
+  raise "missing argument for 'large' or 'small'"
 end
 
-# todo - process doesn't break for failed simulations, but some failures to break it, such as invalid argument values for measures.
+if Dir.exist?(OUTPUT_PATH)
+  puts "removing directories in: #{OUTPUT_PATH}"
+  FileUtils.rm_rf(OUTPUT_PATH)
+end
 
-# run the jobs
-# if gem parallel isn't installed then comment out this could and use system(string) to run one job at a time
-require 'parallel'
-num_parallel = 11 # 6 on mac 11 on pc
-Parallel.each(jobs, in_threads: num_parallel) do |job|
-  puts job
+puts "creating directories in: #{OUTPUT_PATH}"
+osw = File.read("#{OSW_TEMPLATE}")
+osw_jobs = []
+fixtures.each do |f|
+  output_dir = "#{OUTPUT_PATH}/#{f}"
+  osw_path = "#{output_dir}/#{TEST_TYPE}.osw"
+  FileUtils.mkdir_p(output_dir)
+  File.write(osw_path, osw.gsub('GBXML_INPUT.xml', f))
+  osw_jobs << "openstudio run -w \"#{osw_path}\""
+end
+
+Parallel.each(osw_jobs, in_threads: 11) do |job|
+  start = Time.now
+  osw = "#{job.gsub('openstudio run -w', '')}"
+  puts osw
   system(job)
+  stop = Time.now
+  puts "#{osw}: #{(stop - start).to_i} s"
 end
